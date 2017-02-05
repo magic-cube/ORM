0f0fdb7bee9ad0eec3b096951e6b88e3c77231ab
magic-cube <15619225472@163.com>
2017-02-05 21:34:34 +0800

    负责java数据类型和数据库数据类型的相互转换

---
+++ src/sorm/core/TypeConvertor.java
@@ -0,0 +1,25 @@
+package sorm.core;
+/**
+ * 负责java数据类型和数据库数据类型的相互转换
+ * @author hc
+ *
+ */
+public interface TypeConvertor {
+	
+	/**
+	 * 将数据库数据类型转换成java的数据类型
+	 * @param columnType 数据库字段的数据类型
+	 * @return java的数据类型
+	 */
+	public String databaseType2JavaType(String columnType);
+	
+	
+	/**
+	 * 将java数据类型转换成数据库数据类型
+	 * @param javaDataType java数据类型
+	 * @return 数据库数据类型
+	 */
+	public String javaType2DatabaseType(String javaDataType);
+	
+	
+}
